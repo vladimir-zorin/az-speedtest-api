c634bf4f37f79632872fe54dbe130ab528a6c67c
Vladimir Zorin <vzo@computas.com>
2020-02-12 10:29:16 +0100

    initial commit

---
+++ SpeedTestApi/Controllers/SpeedTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SpeedTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SpeedTestController : ControllerBase
+    {
+        // GET speedtest/ping
+        [Route("ping")]
+        [HttpGet]
+        public string Ping()
+        {
+            return "PONG";
+        }
+    }
+}
